a0a198ee98abd80249b9e0dfea57dda22271d6f6
Jonathan Warner <warnerjon12@gmail.com>
2024-09-30 12:32:42 -0600

    Added/updated files for lab 4

---
+++ src/fifo.c
@@ -0,0 +1,5 @@
+#include "fifo.h"
+
+void fifo_worker_handler(QueueHandle_t requests, QueueHandle_t results, int id) {
+    // TODO
+}
---
+++ src/signal.c
@@ -0,0 +1,13 @@
+#include "signal.h"
+
+void signal_handle_calculation(SemaphoreHandle_t request,
+                               SemaphoreHandle_t response,
+                               struct signal_data *data) {
+    // TODO
+}
+
+BaseType_t signal_request_calculate(SemaphoreHandle_t request,
+                                    SemaphoreHandle_t response,
+                                    struct signal_data *data) {
+    // TODO
+}
