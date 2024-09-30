6365501c26277101e98c9869a8d4985cbfc48bdd
Jonathan Warner <warnerjon12@gmail.com>
2024-09-30 12:42:48 -0600

    Implemented worker and request functions in signal.c

--- src/signal.c
+++ src/signal.c
@@ -3,11 +3,14 @@
 void signal_handle_calculation(SemaphoreHandle_t request,
                                SemaphoreHandle_t response,
                                struct signal_data *data) {
-    // TODO
+    xSemaphoreTake(request, portMAX_DELAY);
+    data->output = data->input + 5;
+    xSemaphoreGive(response);
 }
 
 BaseType_t signal_request_calculate(SemaphoreHandle_t request,
                                     SemaphoreHandle_t response,
                                     struct signal_data *data) {
-    // TODO
+    xSemaphoreGive(request);
+    return xSemaphoreTake(response, portMAX_DELAY);
 }
