c12ee367c233d5c9c8ab75737e3e4d9bd220fe93
Jonathan Warner <warnerjon12@gmail.com>
2024-10-02 17:29:04 -0600

    Added error checking and reporting

--- src/fifo.c
+++ src/fifo.c
@@ -3,9 +3,16 @@
 void fifo_worker_handler(QueueHandle_t requests, QueueHandle_t results, int id) {
     struct request_msg data;
     for (;;) {
-        xQueueReceive(requests, &data, portMAX_DELAY);
-        data.output = data.input + 5;
-        data.handled_by = id;
-        xQueueSendToBack(results, &data, portMAX_DELAY);
+        if (xQueueReceive(requests, &data, portMAX_DELAY) == pdTRUE) {
+            data.output = data.input + 5;
+            data.handled_by = id;
+            if (xQueueSendToBack(results, &data, portMAX_DELAY) != pdTRUE) {
+                printf("Failed to send data:\n\tInput: %d\n\tOutput: %d\n\tHandled by: %u\n",
+                       data.input, data.output, data.handled_by);
+            }
+        }
+        else {
+            printf("Failed to receive request (id=%d)\n", id);
+        }
     }
 }
--- src/signal.c
+++ src/signal.c
@@ -3,14 +3,22 @@
 void signal_handle_calculation(SemaphoreHandle_t request,
                                SemaphoreHandle_t response,
                                struct signal_data *data) {
-    xSemaphoreTake(request, portMAX_DELAY);
-    data->output = data->input + 5;
-    xSemaphoreGive(response);
+    if (xSemaphoreTake(request, portMAX_DELAY) == pdTRUE) {
+        data->output = data->input + 5;
+        if (xSemaphoreGive(response) != pdTRUE) {
+            printf("Failed to release response semaphore!\n");
+        }
+    }
+    else {
+        printf("Failed to obtain request semaphore!\n");
+    }
 }
 
 BaseType_t signal_request_calculate(SemaphoreHandle_t request,
                                     SemaphoreHandle_t response,
                                     struct signal_data *data) {
-    xSemaphoreGive(request);
+    if (xSemaphoreGive(request) != pdTRUE) {
+        printf("Failed to release request semaphore!\n");
+    }
     return xSemaphoreTake(response, 100);
 }
