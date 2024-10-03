d97b6a6e94b679f3f9ce724d7918cfb2a0324291
Jonathan Warner <warnerjon12@gmail.com>
2024-10-02 18:20:56 -0600

    Implemented fifo_worker_handler

--- src/fifo.c
+++ src/fifo.c
@@ -1,5 +1,11 @@
 #include "fifo.h"
 
 void fifo_worker_handler(QueueHandle_t requests, QueueHandle_t results, int id) {
-    // TODO
+    struct request_msg data;
+    for (;;) {
+        xQueueReceive(requests, &data, portMAX_DELAY);
+        data.output = data.input + 5;
+        data.handled_by = id;
+        xQueueSendToBack(results, &data, portMAX_DELAY);
+    }
 }
